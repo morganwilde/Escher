8bb23c37c9f4622374226c7de28844e07808ae2c
Morgan Wilde <wilde.morgan@gmail.com>
2015-11-24 16:11:06 -0800

    Created fragments for all the screens

---
+++ app/src/main/java/co/developertime/android/escher/AnalysisFragment.java
@@ -0,0 +1,27 @@
+package co.developertime.android.escher;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by morganwilde on 24/11/2015.
+ */
+public class AnalysisFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/co/developertime/android/escher/MapFragment.java
@@ -0,0 +1,9 @@
+package co.developertime.android.escher;
+
+import android.app.Fragment;
+
+/**
+ * Created by morganwilde on 24/11/2015.
+ */
+public class MapFragment extends Fragment {
+}
